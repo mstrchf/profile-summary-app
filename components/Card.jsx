66e59a6f97c58d3329fd148c8ba03f2d63410587
pho <phonum5@gmail.com>
2022-05-05 00:09:40 +0000

    add gesture handler for android swipeable compatibility

--- components/Card.jsx
+++ components/Card.jsx
@@ -1,66 +1,71 @@
 import React from "react";
 import { View, StyleSheet, Image, Text, TouchableOpacity } from "react-native";
 import { MaterialCommunityIcons, AntDesign } from "@expo/vector-icons";
-import { Swipeable } from "react-native-gesture-handler";
+import {
+  Swipeable,
+  GestureHandlerRootView,
+} from "react-native-gesture-handler";
 const profile = {
   uri: "https://images.unsplash.com/photo-1522075469751-3a6694fb2f61?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=880&q=80",
 };
 
 const Card = ({ darkMode }) => {
   return (
-    <Swipeable
-      renderRightActions={() => {
-        return (
-          <TouchableOpacity
-            activeOpacity={0.75}
-            style={[
-              styles.rightAction,
-              { backgroundColor: darkMode ? "#640207" : "#FC5F67" },
-            ]}
-          >
-            <AntDesign
-              name="delete"
-              size={34}
-              color={darkMode ? "white" : "black"}
-            />
-          </TouchableOpacity>
-        );
-      }}
-      renderLeftActions={() => {
-        return (
-          <TouchableOpacity
-            activeOpacity={0.75}
-            style={[
-              styles.leftAction,
-              { backgroundColor: darkMode ? "#002929" : "#C2FFED" },
-            ]}
-          >
-            <MaterialCommunityIcons
-              name="archive-outline"
-              size={34}
-              color={darkMode ? "white" : "black"}
-            />
-          </TouchableOpacity>
-        );
-      }}
-    >
-      <View style={[styles.container]}>
-        <Image
-          source={profile}
-          style={{ width: 60, height: 60, borderRadius: 30 }}
-        />
-        <View style={styles.text}>
-          <Text style={{ fontSize: 16, fontWeight: "700" }}>
-            Trip to Valley of Kings
-          </Text>
-          <Text>Giza - Egypt</Text>
-          <View style={styles.date}>
-            <MaterialCommunityIcons name="clock" size={15} color="#999" />
-            <Text style={{ color: "#aaa", marginLeft: 5 }}>17/08/2019</Text>
+    <GestureHandlerRootView>
+      <Swipeable
+        renderRightActions={() => {
+          return (
+            <TouchableOpacity
+              activeOpacity={0.75}
+              style={[
+                styles.rightAction,
+                { backgroundColor: darkMode ? "#640207" : "#FC5F67" },
+              ]}
+            >
+              <AntDesign
+                name="delete"
+                size={34}
+                color={darkMode ? "white" : "black"}
+              />
+            </TouchableOpacity>
+          );
+        }}
+        renderLeftActions={() => {
+          return (
+            <TouchableOpacity
+              activeOpacity={0.75}
+              style={[
+                styles.leftAction,
+                { backgroundColor: darkMode ? "#002929" : "#C2FFED" },
+              ]}
+            >
+              <MaterialCommunityIcons
+                name="archive-outline"
+                size={34}
+                color={darkMode ? "white" : "black"}
+              />
+            </TouchableOpacity>
+          );
+        }}
+      >
+        <View style={[styles.container]}>
+          <Image
+            source={profile}
+            style={{ width: 60, height: 60, borderRadius: 30 }}
+          />
+          <View style={styles.text}>
+            <Text style={{ fontSize: 16, fontWeight: "700" }}>
+              Trip to Valley of Kings
+            </Text>
+            <Text>Giza - Egypt</Text>
+            <View style={styles.date}>
+              <MaterialCommunityIcons name="clock" size={15} color="#999" />
+              <Text style={{ color: "#aaa", marginLeft: 5 }}>17/08/2019</Text>
+            </View>
           </View>
         </View>
-      </View>
-    </Swipeable>
+      </Swipeable>
+    </GestureHandlerRootView>
   );
 };
 
@@ -76,14 +81,14 @@ const styles = StyleSheet.create({
     borderTopRightRadius: 40,
     borderBottomRightRadius: 40,
     backgroundColor: "#fff",
-    shadowColor: "#aaa",
+    shadowColor: "#000",
     shadowOffset: {
       width: 0,
       height: 0,
     },
-    shadowOpacity: 0.5,
-    shadowRadius: 5,
-    elevation: 15,
+    shadowOpacity: 0.25,
+    shadowRadius: 2.5,
+    elevation: 5,
   },
 
   text: {
@@ -97,17 +102,17 @@ const styles = StyleSheet.create({
   },
 
   rightAction: {
-    width: 75,
+    width: 70,
     height: 67.5,
     justifyContent: "center",
     alignItems: "center",
     backgroundColor: "#c00021",
     marginVertical: 10,
     marginRight: 15,
-    borderRadius: 600,
+    borderRadius: 60,
   },
   leftAction: {
-    width: 75,
+    width: 70,
     height: 67.5,
     justifyContent: "center",
     alignItems: "center",
